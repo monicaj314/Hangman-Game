653c64bb208e13ed295a106fe4982a98a30ed989
Monica Jones <monicaj314@yahoo.com>
2017-02-10 15:27:23 -0600

    added score and init state

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,19 +7,15 @@ var wrongGuess = [];
 var newAnswer = "";
 var winCount = 0;
 var lossCount = 0;
+var maxGuesses = 7;
+var gameOver = false;
 
-//blank out letters of the answer
-	for (var i = 0; i < answer.length; i++) {
-		newAnswer = newAnswer + "_"
-	} 
-	var newAnswerDiv = $("<div id='newAnswerDiv'></div>");;
-	$("#answer").append(newAnswerDiv);
-	$("#newAnswerDiv").append(newAnswer);
-	console.log(answer);
+init();
 
 //wrong guesses collected in a separate container. Right guesses fill in the blanks
 
 $("body").on("keypress", function(e) {
+	if (!gameOver) {
 	var guess = e.key;
 	var alreadyGuessed = wrongGuess.indexOf(guess);
 	var rightGuess = answer.indexOf(guess);
@@ -27,31 +23,62 @@ $("body").on("keypress", function(e) {
 		newAnswer = replaceEmptyLetter(newAnswer, guess, rightGuess, answer);
 		$("#newAnswerDiv").text(newAnswer);
 	} else if (alreadyGuessed == -1) {
+		maxGuesses--;
 		wrongGuess.push(e.key);
-        $("#guessedWrong").append(e.key + " ");
+        $("#guessedWrong").text("Letters Guessed: " + wrongGuess.join(", "));
         console.log(wrongGuess);
         console.log(e.key);
     } else {
-    	$("#warning").text("You've already guessed " + e.key + ". Try another letter!").fadeIn().fadeOut(1000);
-    }
-	
+    	$("#warning").text("You've already guessed " + e.key + ", and it was wrong. Try another letter!").fadeIn(250).fadeOut(3000);
+    	}
+	}	
 })
-		function replaceEmptyLetter(word, letter, index, replaceMultiple) {
-	var result = word.substr(0,index) + letter + word.substr(index + 1, word.length);
+	$("body").on("keyup", function() {
+	if (gameOver) {
+		init();
+	}
+	if (newAnswer === answer) {
+    	// alert("You WON!");
+    	winCount++;
+    	gameOver=true;
+	} 	else if (maxGuesses === 0) {
+		// alert("You LOST!");
+		lossCount++ ;
+		gameOver=true;
+	}
+		$("#winCount").text(winCount);
+		$("#lossCount").text(lossCount);
+	})
+function init() {
+	answer = wordBank[Math.floor(Math.random() * wordBank.length)];
+	wrongGuess = [];
+	newAnswer = "";
+	maxGuesses = 7;
+	gameOver = false;
+	for (var i = 0; i < answer.length; i++) {
+		newAnswer = newAnswer + "_";
+	} 
+	var newAnswerDiv = $('<div id="newAnswerDiv"></div>');
+	$("#answer").text("Guess the letters!");
+	$("#answer").append(newAnswerDiv);
+	$("#newAnswerDiv").append(newAnswer);
+	$("#guessedWrong").text("Letters Guessed: ");
+	console.log(answer);
+}
+
+function replaceEmptyLetter(word, letter, index, replaceMultiple) {
+	var result = word;
 	if(replaceMultiple) { // determines if the letter guessed replaces multiple blanks
 		for(var i=0; i < replaceMultiple.length; i++) {
-				if(replaceMultiple[i] === letter) {
-					result = replaceEmptyLetter(result, letter, i);
-				}
+			if(replaceMultiple[i] === letter) {
+			result = replaceEmptyLetter(result, letter, i);
 			}
+		}
 
+	} else {
+		result = word.substr(0,index) + letter + word.substr(index + 1, word.length);
 		}
 		return result
 	}
-	$("body").on("keyup", function() {
-	if (newAnswer === answer) {
-    	alert("You WIN!");
-    	winCount = winCount + 1;
-	}
-	})
+
 
